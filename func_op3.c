8da6cf286c1d8c261207ee45b0ff9f3ea2e54eec
Nicolas <nicolasherrerac20@gmail.com>
2020-12-21 15:48:37 +0000

    more functions

--- func_op3.c
+++ func_op3.c
@@ -23,3 +23,103 @@ void add(stack_t **s_head, unsigned int line_number)
 	(*s_head)->prev = NULL;
 	free(temp);
 }
+/**
+ * sub - subtracts the top element of the stack
+ * from the second top element of the stack.
+ * @s_head: First node.
+ * @line_number: Currrent line number.
+ */
+void sub(stack_t **s_head, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (s_head == NULL)
+		return;
+
+	if (*s_head == NULL || (*s_head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		return (EXIT_FAILURE);
+	}
+
+	(*s_head)->next->n -= (*s_head)->n;
+	temp = *s_head;
+	*s_head = (*s_head)->next;
+	(*s_head)->prev = NULL;
+	free(temp);
+}
+/**
+ * div -  divides the second top element of the
+ * stack by the top element of the stack.
+ * @s_head: First node.
+ * @line_number: Currrent line number.
+ */
+void div(stack_t **s_head, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (s_head == NULL)
+		return;
+
+	if (*s_head == NULL || (*s_head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		return (EXIT_FAILURE);
+	}
+
+	(*s_head)->next->n /= (*s_head)->n;
+	temp = *s_head;
+	*s_head = (*s_head)->next;
+	(*s_head)->prev = NULL;
+	free(temp);
+}
+/**
+ * mul - multiplies the second top element of the
+ * stack with the top element of the stack.
+ * @s_head: First node.
+ * @line_number: Currrent line number.
+ */
+void mul(stack_t **s_head, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (s_head == NULL)
+		return;
+
+	if (*s_head == NULL || (*s_head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		return (EXIT_FAILURE);
+	}
+
+	(*s_head)->next->n *= (*s_head)->n;
+	temp = *s_head;
+	*s_head = (*s_head)->next;
+	(*s_head)->prev = NULL;
+	free(temp);
+}
+/**
+ * mod - computes the rest of the division of the
+ * second top element of the stack by the top element of the stack.
+ * @s_head: First node.
+ * @line_number: Currrent line number.
+ */
+void mod(stack_t **s_head, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (s_head == NULL)
+		return;
+
+	if (*s_head == NULL || (*s_head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		return (EXIT_FAILURE);
+	}
+
+	(*s_head)->next->n %= (*s_head)->n;
+	temp = *s_head;
+	*s_head = (*s_head)->next;
+	(*s_head)->prev = NULL;
+	free(temp);
+}
